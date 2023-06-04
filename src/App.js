4382837f63106ff9539f34eb75fb101319af384f
mlekhi <maya.lekhi1@gmail.com>
2023-06-03 23:41:24 -0400

    main functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+
+function NavBar() {
+  return (
+    <section id="navbar">
+      <div className="left-content">
+        <a>Insert name</a>
+      </div>
+      <div className="menu-content">
+        <span>
+          <a>Page</a>
+          <a>Page</a>
+          <a>Page</a>
+          <a>Log in</a>
+        </span>
+      </div>
+    </section>
+  );
+}
+
+function Footer() {
+  return (
+    <section id="navbar">
+      <div className="left-content">
+        <a>Insert name</a>
+      </div>
+      <div className="menu-content">
+        <span>
+          <a>Contact Info</a>
+        </span>
+      </div>
+    </section>
+  );
+}
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          coffee chat question here
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button>New Question</button>
+        <div className="menu-content">
+          <button>Upvote</button>
+          <button>Downvote</button>
+        </div>
       </header>
+      <Footer />
     </div>
   );
 }
