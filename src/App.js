73a3010bb36e87d6073b01db7981202e225d7e5d
mlekhi <maya.lekhi1@gmail.com>
2023-06-05 17:12:52 -0400

    base web layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function NavBar() {
   return (
     <section id="navbar">
       <div className="left-content">
-        <a>Insert name</a>
+        <a href="logo">Insert name</a>
       </div>
       <div className="menu-content">
         <span>
-          <a>Page</a>
-          <a>Page</a>
-          <a>Page</a>
-          <a>Log in</a>
+          <a href="tbd">Page</a>
+          <a href="tbd">Page</a>
+          <a href="tbd">Page</a>
+          <a href="tbd">Log in</a>
         </span>
       </div>
     </section>
@@ -22,11 +24,11 @@ function Footer() {
   return (
     <section id="navbar">
       <div className="left-content">
-        <a>Insert name</a>
+        <a href="logo">Insert name</a>
       </div>
       <div className="menu-content">
         <span>
-          <a>Contact Info</a>
+          <a href="tbd">Contact Info</a>
         </span>
       </div>
     </section>
@@ -34,14 +36,35 @@ function Footer() {
 }
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('http://127.0.0.1:8000');
+      setData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      console.error('Error message:', error.message);
+    }
+  };
+
   return (
     <div className="App">
       <NavBar />
       <header className="App-header">
-        <p>
-          coffee chat question here
-        </p>
-        <button>New Question</button>
+        {/* ok so i don't really know what's going on down there */}
+        {data ? (
+          <div>
+            <pre>{JSON.stringify(data, null, 2)}</pre>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+        <button onClick={fetchData}>New Question</button>
         <div className="menu-content">
           <button>Upvote</button>
           <button>Downvote</button>
